5cf33a3b1781af6e0f7f69c0b04847218985440b
Manoj Eliger <eligermanoj@gmail.com>
2025-09-03 06:53:57 +0530

    fixed delete button logic

--- Todo_app/app.js
+++ Todo_app/app.js
@@ -1,17 +1,32 @@
-let task= document.querySelector("input");
-let list= document.querySelector("ul");
-let btn= document.querySelector("button");
- btn.addEventListener("click",function () {
+let task= document.querySelector("input");//taken input as task
+let list= document.querySelector("ul");//variable to access ul
+let btn= document.querySelector("button");//variable to access button(add task)
+
+ btn.addEventListener("click",function () { // as we click on add Task, creats a list element,stores input task.
     let item= document.createElement("li");
     item.innerText= task.value;
-    list.appendChild(item);
-    console.log("Task added successfully\n");
-    task.value=""; 
+
+    let delbtn= document.createElement("button");//creating delete button to delete task if we won't need.
+    delbtn.innerText= "delete";
+    delbtn.classList.add("delete");//creates a class delete in HTML.
+
+    item.appendChild(delbtn); //adds delete button for each task .(item)
+    list.appendChild(item); //list will updated with every new input .
+    task.value=""; //it will make empty the input box after add task.
  });
 
-// for (let items of list) {
+let ul = document.querySelector("ul");
+ ul.addEventListener("click", function(event){
+   if  (event.target.nodeName =="BUTTON"){
+   let listItem = event.target.parentElement;
+   listItem.remove();
+   console.log("deleted");
+} });
 
+// let delbtns=document.querySelectorAll(".delete");
+// for (delbtn of delbtns) {
+//    delbtn.addEventListener("click",function() {
+//       let par=this.parentElement
+//    })
+//}
 
-//     console.log (task.value);
-
-//  }
